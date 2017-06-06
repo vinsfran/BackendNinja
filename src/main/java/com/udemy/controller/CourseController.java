e765f6bbfaa721be2f79c2d6fb949637023fe284
Vicente Insfrán C. <vinsfran@gmail.com>
2017-06-06 18:26:43 -0400

    Creando e integrando todas las capas - La vista (Parte 2)

--- src/main/java/com/udemy/controller/CourseController.java
+++ src/main/java/com/udemy/controller/CourseController.java
@@ -29,6 +29,7 @@ public class CourseController {
     public ModelAndView listAllCourses() {
         LOG.info("Call: listAllCourses()");
         ModelAndView mav = new ModelAndView(COURSES_VIEW);
+        mav.addObject("course", new Course());
         mav.addObject("courses", courseService.listAllCourses());
         return mav;
     }
