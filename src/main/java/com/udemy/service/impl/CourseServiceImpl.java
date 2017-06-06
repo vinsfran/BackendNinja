2f9b0898379a0b8a9f8f21c4c3446e9651e1b264
Vicente Insfran <vinsfran@gmail.com>
2017-06-06 13:23:14 -0400

    Creando e integrando todas las capas - El servicio

---
+++ src/main/java/com/udemy/service/impl/CourseServiceImpl.java
@@ -0,0 +1,37 @@
+package com.udemy.service.impl;
+
+import com.udemy.entity.Course;
+import com.udemy.repository.CourseJpaRepository;
+import com.udemy.service.CourseService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    @Qualifier("courseJpaRepository")
+    private CourseJpaRepository courseJpaRepository;
+
+    @Override
+    public List<Course> listAllCourses() {
+        return courseJpaRepository.findAll();
+    }
+
+    @Override
+    public Course addCourse(Course course) {
+        return courseJpaRepository.save(course);
+    }
+
+    @Override
+    public int removeCourse(int id) {
+        courseJpaRepository.delete(id);
+        return 0;
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+         return courseJpaRepository.save(course);
+    }
+
+}
