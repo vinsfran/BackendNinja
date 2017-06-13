cc2e21c0764da1bf7b8c30eb5e0297fcb4e48398
Vicente Insfran <vinsfran@gmail.com>
2017-06-13 10:23:10 -0400

    Sobreescribiendo el método UserService

---
+++ src/main/java/com/udemy/service/UserService.java
@@ -0,0 +1,46 @@
+package com.udemy.service;
+
+import com.udemy.entity.UserRole;
+import com.udemy.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    @Qualifier
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.udemy.entity.User user = userRepository.findByUserName(username);
+        List<GrantedAuthority> authorities = buildAuthorities(user.getUserRole());
+        return buildUser(user, authorities);
+    }
+
+    private User buildUser(com.udemy.entity.User user, List<GrantedAuthority> authorities) {
+        return new User(user.getUsername(), user.getPassword(), user.isEnabled(),
+                true, true, true, authorities);
+    }
+
+    private List<GrantedAuthority> buildAuthorities(Set<UserRole> userRoles) {
+        Set<GrantedAuthority> auths = new HashSet<GrantedAuthority>();
+        for (UserRole userRole : userRoles) {
+            auths.add(new SimpleGrantedAuthority(userRole.getRole()));
+        }
+        return new ArrayList<GrantedAuthority>(auths);
+    }
+
+}
