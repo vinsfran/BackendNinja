be816d63601d50a7823b3c5eefcb77c54d3413f2
Vicente Insfran <vinsfran@gmail.com>
2017-06-14 13:27:03 -0400

    Guardando logs de petición en base de datos

--- src/main/java/com/udemy/component/RequestTimeInterceptor.java
+++ src/main/java/com/udemy/component/RequestTimeInterceptor.java
@@ -1,17 +1,27 @@
 package com.udemy.component;
 
+import com.udemy.repository.LogRepository;
+import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 
+    @Autowired
+    @Qualifier("logRepository")
+    private LogRepository logRepository;
+
     public static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
-    
+
     //PRIMERO
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -23,7 +33,14 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         long startTime = (long) request.getAttribute("startTime");
-        LOG.info("Url to: '" + request.getRequestURL().toString() + "' in: '" + (System.currentTimeMillis() - startTime) + "ms'");
+        String url = request.getRequestURL().toString();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = "";
+        if (auth != null && auth.isAuthenticated()) {
+            username = auth.getName();
+        }
+        logRepository.save(new com.udemy.entity.Log(new Date(), auth.getDetails().toString(), username, url));
+        LOG.info("Url to: '" + url + "' in: '" + (System.currentTimeMillis() - startTime) + "ms'");
     }
 
 }
---
+++ src/main/java/com/udemy/repository/LogRepository.java
@@ -0,0 +1,11 @@
+package com.udemy.repository;
+
+import com.udemy.entity.Log;
+import java.io.Serializable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("logRepository")
+public interface LogRepository extends JpaRepository<Log, Serializable>{
+
+}
